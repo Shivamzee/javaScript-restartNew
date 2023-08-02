bc347e2fdbfbfd169ea65bc809ff231bbc6b7d93
Shivamzee <116863690+Shivamzee@users.noreply.github.com>
2023-08-02 12:31:25 +0000

    Variable is done

---
+++ javascript_basic/jsVariable.js
@@ -0,0 +1,14 @@
+const accountId = 235412;
+let accountEmail = "shivam132@gmail.com" ;
+var accounPassword = 35412;
+accountId = "Dholakpur" ;
+
+// accountId = 72427  is not allowed
+
+accountEmail = "shivagen@gmail.com";
+accounPassword = 37261 ;
+accountId = "Maxico" ;
+
+console.log(accountId)
+
+console.table([accountId , accountEmail , accounPassword , accountId  ])
