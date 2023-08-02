269211ce514dcd4b2ac88ee77b246693bfda0abb
Shivamzee <116863690+Shivamzee@users.noreply.github.com>
2023-08-02 15:55:58 +0000

    variable is done

--- javascript_basic/jsVariable.js
+++ javascript_basic/jsVariable.js
@@ -1,13 +1,13 @@
 const accountId = 235412;
 let accountEmail = "shivam132@gmail.com" ;
 var accounPassword = 35412;
-accountId = "Dholakpur" ;
+accountCity = "Dholakpur" ;
 
 // accountId = 72427  is not allowed
 
 accountEmail = "shivagen@gmail.com";
 accounPassword = 37261 ;
-accountId = "Maxico" ;
+accountCity = "Maxico" ;
 
 console.log(accountId)
 
