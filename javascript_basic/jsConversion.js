76f780206b8d2b8c50eb10daf5fe7bb82beafcc3
Shivamzee <116863690+Shivamzee@users.noreply.github.com>
2023-08-04 02:13:48 +0000

    jsDatatype Conversion is done

---
+++ javascript_basic/jsConversion.js
@@ -0,0 +1,71 @@
+let score = 88 ;
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+
+ score = "55";
+//  console.log(typeof score);
+//  console.log(typeof (score));
+
+ let valueInNumber = Number(score)
+//  console.log(typeof valueInNumber , score);
+//  console.log(typeof (score));
+
+
+
+
+
+// Learn about null
+let checkTypeOfNull = null
+// console.log(typeof checkTypeOfNull ,  checkTypeOfNull);
+
+checkTypeOfNull = Number(checkTypeOfNull)
+// console.log(typeof checkTypeOfNull ,  checkTypeOfNull);
+
+
+
+
+
+// Now learn about undefind
+let checkTpyeOfundefind = undefined ;
+//console.log(checkTpyeOfundefind , typeof checkTpyeOfundefind);
+
+let convertUndefindIntoNumber = Number (checkTpyeOfundefind);
+// console.log(convertUndefindIntoNumber , typeof convertUndefindIntoNumber);
+
+
+
+
+
+
+
+// Now Learn about boolean type
+let checkTypeOfBooleanValue = true ;
+// console.log(checkTypeOfBooleanValue , typeof checkTypeOfBooleanValue);
+
+let convertBooleanIntoNumber = Number(checkTypeOfBooleanValue)
+// console.log(convertBooleanIntoNumber , typeof convertBooleanIntoNumber);
+
+
+
+let checkTypeOfBooleanValueFalse = false ;
+// console.log(checkTypeOfBooleanValue , typeof checkTypeOfBooleanValue);
+
+let convertBooleanfalseIntoNumber = Number(checkTypeOfBooleanValue)
+// console.log(convertBooleanIntoNumber , typeof convertBooleanIntoNumber);
+
+
+
+
+
+// Let convert into string..
+let numberToConvertIntoString =  55;
+ console.log(numberToConvertIntoString , typeof numberToConvertIntoString ); // 
+
+let convertNumberIntoString = String(numberToConvertIntoString);
+console.log(convertNumberIntoString , typeof convertNumberIntoString );
+
+
+
+
+
