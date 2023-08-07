ca11049eb6da005a7ca0800c5409f6a2f1e64fa2
Shivamzee <116863690+Shivamzee@users.noreply.github.com>
2023-08-07 09:25:47 +0000

    on nit equal to opreator

--- javascript_basic/jsComparison.js
+++ javascript_basic/jsComparison.js
@@ -1,3 +1,17 @@
-// ----------------- JavaScript Comparison Operators-----------------------
-/*Comparison operators compare two values and give back a boolean value: either true or false. Comparison operators are used in decision making and loops.*/
+//--------------------------Comparison and Logical Operators------------------------
+//(1)   ==	Equal to: true if the operands are equal
+//Example:
+//console.log(2 == '2');   // true
+//console.log("Shivam" == 'shivam');  // false
+//console.log(0 == false); //true
 
+
+
+//(2)  ===	Strict equal to: true if the operands are equal and of the same type.
+//Example:
+//console.log(2 ==="2");//false
+//console.log("Shivam"==="Shivam"); //true
+//console.log(0===false);//false
+
+
+//(3) !=	Not equal to: true if the operands are not equal.
