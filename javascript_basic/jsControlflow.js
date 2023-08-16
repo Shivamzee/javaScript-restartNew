c1a5f16a0736ff60c97cf87425a54896ba1a254f
Shivamzee <116863690+Shivamzee@users.noreply.github.com>
2023-08-16 05:22:29 +0000

    if elase done.

---
+++ javascript_basic/jsControlflow.js
@@ -0,0 +1,77 @@
+/////JavaScript if Statement
+// Example 
+// const studentName = "Rituraj";
+// const knownCodingLanguage = "Redux";
+// if (knownCodingLanguage ==="Redux") {
+//     console.log(`${studentName} ,You are qualified`)
+// }
+// console.log("Sorry , first you all have to learn Redux");
+
+
+// const studentName = "Abhi Dixit";
+// const knownCodingLanguage = "Java";
+// if (knownCodingLanguage ==="Redux") {
+//     console.log(`${studentName} ,You are qualified`)
+// }
+// console.log("Sorry , first you all have to learn Redux");
+
+
+
+
+
+/// JavaScript if...else statement
+// Example
+// const studentName = "Shivam" ;
+// const canSpeakEnglish = 'Yes';
+// if (canSpeakEnglish ==="Yes") {
+//      console.log(`it's Good ${studentName}. Learn more`);
+// } else {
+//     console.log(`Why are you afraid, ${studentName}. If you don't know, learn it. You can learn it.`);
+// }
+
+// const studentName = "Shivam" ;
+// const canSpeakEnglish = 'Yes';
+// if (canSpeakEnglish ==="No") {
+//      console.log(`it's Good ${studentName}. Learn more`);
+// } else {
+//     console.log(`Why are you afraid, ${studentName}. If you don't know, learn it. You can learn it.`);
+// }
+
+
+
+///JavaScript if...else if statement
+//case -1
+// const studentName = "Priyanka" ;
+// const collageName = "IGNOU" ;
+// const passingYear = 2024 ;
+// if (collageName === "IGNOU") {
+//     console.log("Upload your id card of IGNOU.");
+// } else if (passingYear === 2024) {
+//        console.log("You are qualified.");
+// } else {
+//     console.log("You are unqualified");
+// } 
+
+// case-2
+// const studentName = "Priyanka" ;
+// const collageName = "IGNOU" ;
+// const passingYear = 2024 ;
+// if (collageName === "Lpu") {
+//     console.log("Upload your id card of IGNOU.");
+// } else if (passingYear === 2024) {
+//        console.log("You are qualified.");
+// } else {
+//     console.log("You are unqualified");
+// } 
+
+// case-3
+// const studentName = "Priyanka" ;
+// const collageName = "IGNOU" ;
+// const passingYear = 2026 ;
+// if (collageName === "Lpu") {
+//     console.log("Upload your id card of IGNOU.");
+// } else if (passingYear === 2024) {
+//        console.log("You are qualified.");
+// } else {
+//     console.log("You are unqualified");
+// } 
