d22592dfc601d690be272cf300a2ceebd7860fde
Shivamzee <116863690+Shivamzee@users.noreply.github.com>
2023-08-19 10:48:56 +0000

    sum program done

---
+++ javascript_basic/jsloop.js
@@ -0,0 +1,41 @@
+// Loops are used to  repeat a block of code
+
+//(1) For Loop
+/*
+// syntax
+for (initialExpression; condition; updateExpression) {
+    // for loop body
+}
+*/
+
+// Example
+// (1)Display a text 10 times
+ const byDrKalam = `I'm not a handsome guy, but I can give my hand to someone who needs help. `
+// for (let i = 0; i<10; i++) {
+//     console.log(byDrKalam);
+    
+// }
+
+
+//(2) Print Numbers ....
+// for(i=0 ; i<=45 ; i++){
+//     if (i==65) {
+//         console.log("Yes it is 1 cr.");
+//     }else if(i==26){
+//         console.log( `${i}  Wait it too big number.`);
+//     }else{
+//         console.log(`${i}  Kya hai , are bhai chate kya ho ?`);
+//     }
+// }
+
+
+
+//(3) Sum of n Natural numbers...
+// let sumOfnNumbers=0 ;
+// let endNumber = 5 ;
+// for(let i =1 ; i<=endNumber; i++  ){
+//      sumOfnNumbers += i ;
+//      console.log(sumOfnNumbers + " " + i);
+// }
+//  console.log(sumOfnNumbers);
+
