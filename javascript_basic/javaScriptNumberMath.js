20b3804b1fa1c508e5113efb16b69c084846218e
Shivamzee <116863690+Shivamzee@users.noreply.github.com>
2023-09-15 03:35:30 +0000

    learning js Math om max

--- javascript_basic/javaScriptNumberMath.js
+++ javascript_basic/javaScriptNumberMath.js
@@ -21,4 +21,24 @@ const averagePeopleIncome = 320000.2589;
 // console.log(squreOfNumber);
 // console.log(Math.pow(3,3));
 
-//(3) Math.ceil()=>
+//(3) Math.ceil()=>mathod rounds a decimal number up to the next largest and returns it.
+// let getNum = 9.3;
+// console.log(getNum);
+// // console.log(Math.ceil(getNum));
+
+// getNum = 3.8;
+// console.log(Math.ceil(getNum));
+
+
+//(4) Math.floor()=> function rounds down a number to the next smallest integer.
+// let downNumber = 4.4;
+// console.log(Math.floor(downNumber));
+
+// downNumber = 7.8;
+// console.log(Math.floor(downNumber));
+
+//(5) Math.max()=> returns the largest value among the given numbers.
+// console.log(Math.max(6,8,4,3));
+// const mArray = [2,14,9,25,4,7];
+// let out = Math.max(...mArray);
+// console.log(out);
