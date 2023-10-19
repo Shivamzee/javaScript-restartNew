1961ef3f80f04132f5d6f071e50ec14eb526eab1
Shivamzee <116863690+Shivamzee@users.noreply.github.com>
2023-10-19 06:01:23 +0000

    yes filter is done

--- jsBasic02/jaArray.js
+++ jsBasic02/jaArray.js
@@ -3,19 +3,40 @@
 
 
 // Q-find the largest numbers from three numbers.
-let largestNum = (a,b,c) =>{
-    let makeArray = [a,b,c];
-    let knowBigNum = makeArray.filter(e =>{
-        if (a >b && a>c ) {
-            console.log(`Largest number is: ${a}`);
-        } else if(b>a && b>c) {
-            console.log(`Largest number is: ${b}`);
-        } else if(c>a && c>b){
-            console.log(`Largest number is: ${c}`);
-        }else{
-            console.log("Try again");
-        }
-    })
-}
+// let largestNum = (a,b,c) =>{
+//     let makeArray = [a,b,c];
+//     let knowBigNum = makeArray.filter(e =>{
+//         if (a >b && a>c ) {
+//             console.log(`Largest number is: ${a}`);
+//         } else if(b>a && b>c) {
+//             console.log(`Largest number is: ${b}`);
+//         } else if(c>a && c>b){
+//             console.log(`Largest number is: ${c}`);
+//         }
+//         else{
+//             console.log("Try again");
+//         }
+//     })
+// }
 
-largestNum("1ss",true,7)
+// largestNum(12,41,0);
+
+// let largestNum = (a,b,c) =>{
+//     let makeArray = [a,b,c];
+//     let knowBigNum = makeArray.filter(e =>{
+//         if (a >=b && a>=c ) {
+//             console.log(`Largest number is: ${a}`);
+//         } else if(b>=a && b>=c) {
+//             console.log(`Largest number is: ${b}`);
+//         } else if(c>=a && c>=b){
+//             console.log(`Largest number is: ${c}`);
+//         }
+//         else{
+//             console.log("Try again");
+//         }
+//     })
+// }
+
+// largestNum(12,45,55);
+
+
